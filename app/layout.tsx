c04f2288a9d23182d92d22a02c7ffa090c90662c
AlexMcNicholl <47499560+AlexMcNicholl@users.noreply.github.com>
2025-02-07 13:37:28 -0500

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,8 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import ParticlesBackground from "@/components/ParticlesBackground"; // Import Particles Background
+import ParticlesBackground from "../components/ParticlesBackground";
+
 import type React from "react"; // Import React
 
 const inter = Inter({ subsets: ["latin"] });
