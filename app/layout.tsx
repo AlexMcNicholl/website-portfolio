a74dfd74badfa1eca24ef92e9f1ef120f8151c4d
AlexMcNicholl <47499560+AlexMcNicholl@users.noreply.github.com>
2025-02-07 13:08:41 -0500

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,30 +1,31 @@
-import { ThemeProvider } from "@/components/theme-provider"
-import { cn } from "@/lib/utils"
-import type { Metadata } from "next"
-import { Inter } from "next/font/google"
-import "./globals.css"
-import type React from "react" // Import React
+import { ThemeProvider } from "@/components/theme-provider";
+import { cn } from "@/lib/utils";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+import ParticlesBackground from "@/components/ParticlesBackground"; // Import Particles Background
+import type React from "react"; // Import React
 
-const inter = Inter({ subsets: ["latin"] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Alex McNicholl - FinTech",
-  description: "Portfolio to showoff my skills",
-}
+  description: "Portfolio to show off my skills",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en" suppressHydrationWarning>
       <body className={cn("min-h-screen bg-background font-sans antialiased", inter.className)}>
         <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
+          <ParticlesBackground /> {/* Added Particles */}
           {children}
         </ThemeProvider>
       </body>
     </html>
-  )
+  );
 }
-
