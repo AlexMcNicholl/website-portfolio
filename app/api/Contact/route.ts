569f198e3911a80b0d445ea33ce299bc047dfa7d
Alex McNicholl <alexmcnicholl@Alexs-MacBook-Air.local>
2025-02-08 18:50:09 -0500

    Resolved merge conflict in Contact API

--- app/api/Contact/route.ts
+++ app/api/Contact/route.ts
@@ -1,10 +1,6 @@
 import { NextResponse } from "next/server";
 
-<<<<<<< HEAD
-export async function POST(req) {
-=======
 export async function POST(req: { json: () => PromiseLike<{ name: any; email: any; message: any; }> | { name: any; email: any; message: any; }; }) {
->>>>>>> fd03747 (Finance Viz Project)
   try {
     const { name, email, message } = await req.json();
     console.log("Received form submission:", { name, email, message });
