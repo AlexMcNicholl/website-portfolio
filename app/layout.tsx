2c50d5aacbce92f4446e8410d503e97d228c5077
AlexMcNicholl <amcnicholl02@gmail.com>
2025-06-04 22:02:50 -0400

    1

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import ParticlesBackground from "./components/ParticlesBackground";
+import { Analytics } from "@vercel/analytics/react";
 
 const inter = Inter({ subsets: ["latin"] });
 
