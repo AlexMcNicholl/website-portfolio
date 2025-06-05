8642b42af817f7e66cfdab35586767c6cf1c9f92
AlexMcNicholl <amcnicholl02@gmail.com>
2025-06-04 22:12:21 -0400

    edit

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,6 +24,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
             {children}
           </SessionProvider>
         </ThemeProvider>
+        <Analytics /> {/* Optional: Add Vercel Analytics */}
       </body>
     </html>
   );
