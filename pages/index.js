990bf286d3d65bccb176c0e42820c0cf16f4f576
AlexMcNicholl <amcnicholl02@gmail.com>
2025-11-27 19:57:42 -0500

    Add root redirect to trading hub

---
+++ pages/index.js
@@ -0,0 +1,16 @@
+import { useEffect } from 'react';
+import { useRouter } from 'next/router';
+
+export default function Home() {
+    const router = useRouter();
+
+    useEffect(() => {
+        router.push('/trading');
+    }, [router]);
+
+    return (
+        <div className="min-h-screen flex items-center justify-center bg-black text-white">
+            <p className="animate-pulse">Loading Command Center...</p>
+        </div>
+    );
+}
